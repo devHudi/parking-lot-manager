4a9791ad910cce968e8cefbfb022f7b37c2e3de5
devHudi <devhudi@gmail.com>
2021-02-12 19:21:34 +0900

    set cra with electron

---
+++ electron/index.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    center: true,
+    resizable: true,
+    fullscreen: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = undefined;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
