2d17e9c6f6196447b135d405ddb5698a6be4018e
devHudi <devhudi@gmail.com>
2021-02-13 21:04:12 +0900

    feat: improve SpaceBetween

--- src/components/SpaceBetween/index.jsx
+++ src/components/SpaceBetween/index.jsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
+import { Space } from "antd";
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
-const SpaceBetween = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
+const SpaceBetween = ({ children, ...props }) => {
+  return <Wrapper {...props}>{children}</Wrapper>;
 };
+
+SpaceBetween.Box = Space;
 
 export default SpaceBetween;
