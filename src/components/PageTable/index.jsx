9538066e89117f20488736563ce06f80f7223e9b
devHudi <devhudi@gmail.com>
2021-02-13 21:04:22 +0900

    feat: implement PageTable

---
+++ src/components/PageTable/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Table, Button } from "antd";
+
+import { SpaceBetween } from "components";
+
+const PageTable = ({
+  name,
+  columns,
+  data,
+  children,
+  onAddClick,
+  onRemoveClick,
+  dataHandler,
+}) => {
+  const [selected, setSelected] = useState([]);
+
+  return (
+    <>
+      <SpaceBetween style={{ marginBottom: "10px" }}>
+        <SpaceBetween.Box>
+          <Button type="primary" onClick={onAddClick}>
+            {name} 추가
+          </Button>
+          <Button type="danger" onClick={() => onRemoveClick(selected)}>
+            선택 삭제
+          </Button>
+        </SpaceBetween.Box>
+        <SpaceBetween.Box>{children}</SpaceBetween.Box>
+      </SpaceBetween>
+      <Table
+        rowSelection={{
+          onChange: (selectedRowKeys, selectedRows) =>
+            setSelected(selectedRows),
+        }}
+        columns={columns}
+        dataSource={dataHandler ? dataHandler(data) : data}
+      />
+    </>
+  );
+};
+
+export default PageTable;
