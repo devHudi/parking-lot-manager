a094ba6afe47befa8740e6f38223233a8c1728de
devHudi <devhudi@gmail.com>
2021-02-13 20:25:39 +0900

    feat: implement SpaceBetween

---
+++ src/components/SpaceBetween/index.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SpaceBetween = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default SpaceBetween;
