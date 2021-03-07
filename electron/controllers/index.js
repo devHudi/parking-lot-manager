651c3b0ea0b670fbd387284720a12511fd4beee5
devHudi <devhudi@gmail.com>
2021-03-07 19:15:44 +0900

    feat: add index.js for Controllers

---
+++ electron/controllers/index.js
@@ -0,0 +1,7 @@
+const Room = require("./Room");
+const Car = require("./Car");
+const PrivateCar = require("./PrivateCar");
+const StakeTransfer = require("./StakeTransfer");
+const PaidTicket = require("./PaidTicket");
+
+module.exports = { Room, Car, PrivateCar, StakeTransfer, PaidTicket };
