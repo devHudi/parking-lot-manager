4d45b31f106d76286f08ece7be568dd1ad3eafa6
devHudi <devhudi@gmail.com>
2021-05-06 23:06:11 +0900

    fix: fix issue caused empty file list from dialog

--- electron/events/fileDialog.js
+++ electron/events/fileDialog.js
@@ -6,14 +6,17 @@ const Controllers = require("../controllers");
 
 const init = () => {
   ipcMain.on("import-spread-sheet", async (event, args) => {
-    const filePath = dialog.showOpenDialogSync({ properties: ["openFile"] })[0];
-    const rawData = fs.readFileSync(filePath);
+    const filePath = dialog.showOpenDialogSync({ properties: ["openFile"] });
 
-    const parsedData = parse(rawData.toString("utf-8"), {
-      skip_empty_lines: true,
-    });
+    if (filePath.length > 0) {
+      const rawData = fs.readFileSync(filePath[0]);
 
-    event.returnValue = await Controllers.Room.importCsv(parsedData);
+      const parsedData = parse(rawData.toString("utf-8"), {
+        skip_empty_lines: true,
+      });
+
+      event.returnValue = await Controllers.Room.importCsv(parsedData);
+    }
   });
 };
 
