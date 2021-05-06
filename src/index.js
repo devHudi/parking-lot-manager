f537963ea54c025f6a318d1154b57cbacd0dba03
devHudi <devhudi@gmail.com>
2021-05-07 02:51:57 +0900

    chore: update todo comments

--- src/index.js
+++ src/index.js
@@ -23,15 +23,20 @@ ReactDOM.render(
 );
 
 /*
+  필수
+  TODO: acc 생성 지난 날짜에도 할 수 있도록 개선
+  TODO: 지분이 -가 될 것 같으면 지분 이전 불가능하도록
+  TODO: 무료주차권 소수점 문제
+  TODO: 고지서 PDF 생성해서 다운로드되는 기능 만들기
+
+*/
+/*
+  부수적 작업
+  TODO: 테이블 있는 페이지마다 날짜, 검색 기능 만들기
   TODO: 모든 action 마다 alert 로 피드백 주기
   TODO: DB Model 에서 모든 필드 default 값 주기
-  TODO: 고지서 PDF 생성해서 다운로드되는 기능 만들기
-  TODO: 테이블 있는 페이지마다 날짜, 검색 기능 만들기
-  TODO: 무료주차권 소수점 문제
-  TODO: 지분이 -가 될 것 같으면 지분 이전 불가능하도록
   TODO: 모든 숫자에 쉼표
   TODO: Redux 로 관리되는 Global 스피너 추가
   TODO: 호실, 개인차량 리스트 리덕스로 관리
   TODO: 모든 통신 비동기로 변경
-  TODO: paidTicket 시간 별 가격
 */
